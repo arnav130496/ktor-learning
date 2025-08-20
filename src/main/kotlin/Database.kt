7c155b24306705c692d8524cbda901428d98df64
Arnav <arnav@Arnavs-MacBook-Air.local>
2025-08-20 12:14:06 +0530

    changes in app.yaml

--- src/main/kotlin/Database.kt
+++ src/main/kotlin/Database.kt
@@ -2,15 +2,23 @@ package com.arnav
 
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
+import io.ktor.server.application.*
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.exposedLogger
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.time.OffsetDateTime
 
-fun configureDatabase() {
+fun Application.configureDatabase() {
     println("Configuring DB Connection")
     val config = HikariConfig().apply {
-        jdbcUrl = System.getenv("JDBC_DATABASE_URL")
-        driverClassName = System.getenv("JDBC_DRIVER")
+        val databaseConfig = environment.config.config("ktor.database")
+
+        // The property names now match your YAML structure
+        jdbcUrl = databaseConfig.property("jdbcUrl").getString()
+        driverClassName = databaseConfig.property("jdbcDriver").getString()
+        username = databaseConfig.property("user").getString()
+        password = databaseConfig.property("password").getString()
+
         isAutoCommit = false
         transactionIsolation = "TRANSACTION_REPEATABLE_READ"
         validate()
@@ -30,7 +38,13 @@ fun isDbConnected(): Boolean {
                 rs.next()
                 rs.getObject(1, String::class.java)
             }
-            exposedLogger.info("Database health check successful: current time is $result")
+            exposedLogger.info("Database health check successful: Dummy data fetched from service_plans table is $result")
+
+            val resultTime = exec("SELECT now() ") { rs ->
+                rs.next()
+                rs.getObject(1, OffsetDateTime::class.java)
+            }
+            exposedLogger.info("Database health check successful: current time is $resultTime")
             true
         }
     } catch (e: Exception) {
